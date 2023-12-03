b276ac09bd30993220f1ee80513a379a42615cdc
fahim <mdfahimhossen629@gmail.com>
2023-12-03 23:27:31 +0800

    First Commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*===================Auto Typed Image==================*/
+var typed = new Typed(".auto-type", {
+    strings: ["I'm Fahim Hossen.", "I'm a Freelancer.", "I'm a Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+})
